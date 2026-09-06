09ebd30952c70f2f8c3a9f46ec38752363591f35
cn-54 <238699711+Cn-54@users.noreply.github.com>
2026-09-06 13:32:33 +0100

    added a loader to load date into memory

---
+++ include/Loader.h
@@ -0,0 +1,8 @@
+#ifndef LOADER_H_
+#define LOADER_H_
+
+#include "MEMORY.h"
+
+void Load_Code(Memory *mem, const char *filename, uint16_t address);
+
+#endif
---
+++ src/Loader.c
@@ -0,0 +1,19 @@
+#include "Loader.h"
+#include <stdio.h>
+
+void Load_Code(Memory *mem, const char *filename, uint16_t address){
+    FILE *file = fopen(filename, "rb");
+
+    if (file == NULL) {
+        return;
+    }
+
+    uint8_t byte;
+
+    while (fread(&byte, 1, 1, file) == 1) {
+        store_byte(mem, address, byte);
+        address++;
+    }
+
+    fclose(file);
+}
