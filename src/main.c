7753de3fc25b5ae609caf8b53e1ddc9603427e31
cn-54 <238699711+Cn-54@users.noreply.github.com>
2026-09-06 16:45:07 +0100

    make it so user can give a program not just a hard coded program

--- src/main.c
+++ src/main.c
@@ -3,18 +3,27 @@
 #include "data.h"
 #include "loader.h"
 #include "memory.h"
+#include <stdio.h>
 
-int main(){
-    
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s <program.bin>\n", argv[0]);
+        return 1;
+    }
     CPU *cpu = Create_CPU();
     Memory *mem = Create_Memory();
 
+    // load vector table and subroutines into memory
     Load_Code(mem, "Code/vector_table/vec_table.bin", VECTORTABLE_START);
     Load_Code(mem, "Code/subroutines/print/print.bin", SUBROUTINES_START);
     Load_Code(mem, "Code/subroutines/strlen/strlen.bin", SUBROUTINES_START*2);
     Load_Code(mem, "Code/subroutines/atoi/atoi.bin", SUBROUTINES_START*3);
     Load_Code(mem, "Code/subroutines/itoa/itoa.bin", SUBROUTINES_START*4);
-    Load_Code(mem, "Code/programs/program.bin", PROGRAM_START);
+    
+    // load user program into memory
+    Load_Code(mem,argv[1],PROGRAM_START);
 
     // cpu->FLAGS |= FLAG_D;
 
